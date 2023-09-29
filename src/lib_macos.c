689cadf60910c486f158018ffcd8a13f8af1859e
liuxichen <710842092@qq.com>
2023-09-29 19:22:32 +0800

    refactor macos implementation for ipv6

--- src/lib_macos.c
+++ src/lib_macos.c
@@ -12,25 +12,36 @@
 #include "share.h"
 #include "lib_macos.h"
 
-// 返回connect导致阻塞的错误码,在macos下为EINPROGRESS
 int m_connect_block_code()
 {
     return EINPROGRESS;
 }
 
-// 返回send导致阻塞的错误码,在macos下为EAGAIN
 int m_send_block_code()
 {
     return EAGAIN;
 }
 
-// 创建kqueue,失败则返回-1,成功则返回fd
+int m_interrupt_code()
+{
+    return EINTR;
+}
+
+int m_ipv4_address_len()
+{
+    return INET_ADDRSTRLEN;
+}
+
+int m_ipv6_address_len()
+{
+    return INET6_ADDRSTRLEN;
+}
+
 int m_kqueue()
 {
     return kqueue();
 }
 
-// 向kqueue中注册事件,失败则返回-1,成功则返回0 changelist应只包含一个元素,如果changelist的flag为EV_ERROR则表示出现了错误
 int m_kevent_ctl(int kq, int fd, int16_t filter, uint16_t flags)
 {
     struct kevent event;
@@ -38,135 +49,163 @@ int m_kevent_ctl(int kq, int fd, int16_t filter, uint16_t flags)
     return kevent(kq, &event, 1, NULL, 0, NULL);
 }
 
-// 阻塞等待事件,失败则返回-1,成功则返回事件个数
 int m_kevent_wait(int kq, struct kevent *eventlist, int nevents, struct timespec *timeout)
 {
     return kevent(kq, NULL, 0, eventlist, nevents, timeout);
 }
 
-// 返回ip地址字节长度
-socklen_t m_address_len()
+int m_ipv4_address_size()
 {
-    return INET_ADDRSTRLEN;
+    return sizeof(struct sockaddr_in);
 }
 
-// 从sockAddr中获取客户端地址字符串,失败则返回-1,成功则返回0
-int m_address(struct sockaddr_in *sockAddr, char *addrStr, socklen_t len)
+int m_ipv6_address_size()
 {
-    const char *ptr = inet_ntop(AF_INET, &(sockAddr->sin_addr), addrStr, len);
-    if (ptr == NULL)
+    return sizeof(struct sockaddr_in6);
+}
+
+int m_get_ipv4_address(struct sockaddr_in *sockAddr, char *addrStr, socklen_t len)
+{
+    const char *result = inet_ntop(AF_INET, &(sockAddr->sin_addr), addrStr, len);
+    if (result == NULL)
     {
         return -1;
     }
     return 0;
 }
 
-// 获取客户端连接的端口号
-uint16_t m_port(struct sockaddr_in *sockAddr)
+int m_get_ipv6_address(struct sockaddr_in6 *sockAddr, char *addrStr, socklen_t len)
+{
+    const char *result = inet_ntop(AF_INET6, &(sockAddr->sin6_addr), addrStr, len);
+    if (result == NULL)
+    {
+        return -1;
+    }
+    return 0;
+}
+
+uint16_t m_ipv4_port(struct sockaddr_in *sockAddr)
 {
     return ntohs(sockAddr->sin_port);
 }
 
-// 创建socket,失败则返回-1,成功则返回fd
-int m_socket_create()
+uint16_t m_ipv6_port(struct sockaddr_in6 *sockAddr)
+{
+    return ntohs(sockAddr->sin6_port);
+}
+
+int m_ipv4_socket_create()
 {
     return socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 }
 
-// 接受socket连接,失败则返回-1,成功则返回socket fd,在非阻塞情况下,未立刻建立的连接错误码为EAGAIN或EWOULDBLOCK
-int m_accept(int socket, struct sockaddr_in *clientAddr, socklen_t clientAddrSize)
+int m_ipv6_socket_create()
+{
+    return socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
+}
+
+int m_accept(int socket, void *clientAddr, socklen_t clientAddrSize)
 {
     return accept(socket, (struct sockaddr *)clientAddr, &clientAddrSize);
 }
 
-// 设置sock地址,失败则返回-1,成功则返回1,地址为非法字符串则返回0
-int m_set_sock_addr(struct sockaddr_in *sockAddr, char *address, uint16_t port)
+int m_set_ipv4_sock_addr(struct sockaddr_in *sockAddr, char *address, uint16_t port)
 {
     sockAddr->sin_family = AF_INET;
     sockAddr->sin_port = htons(port);
-    return inet_pton(AF_INET, address, &(sockAddr->sin_addr));
+    if (address == NULL)
+    {
+        sockAddr->sin_addr.s_addr = INADDR_ANY;
+        return 1;
+    }
+    else
+    {
+        return inet_pton(AF_INET, address, &(sockAddr->sin_addr));
+    }
 }
 
-// 设置reuseAddr选项,失败则返回-1,成功则返回0
+int m_set_ipv6_sock_addr(struct sockaddr_in6 *sockAddr, char *address, uint16_t port)
+{
+    sockAddr->sin6_family = AF_INET6;
+    sockAddr->sin6_port = htons(port);
+    if (address == NULL)
+    {
+        sockAddr->sin6_addr = in6addr_any;
+        return 1;
+    }
+    else
+    {
+        return inet_pton(AF_INET6, address, &(sockAddr->sin6_addr));
+    }
+}
+
 int m_set_reuse_addr(int socket, int value)
 {
     return setsockopt(socket, SOL_SOCKET, SO_REUSEADDR, (void *)&value, sizeof(value));
 }
 
-// 设置keepalive选项,失败则返回-1,成功则返回0
 int m_set_keep_alive(int socket, int value)
 {
     return setsockopt(socket, SOL_SOCKET, SO_KEEPALIVE, (void *)&value, sizeof(value));
 }
 
-// 设置tcpnodelay选项,失败则返回-1,成功则返回0
 int m_set_tcp_no_delay(int socket, int value)
 {
     return setsockopt(socket, IPPROTO_TCP, TCP_NODELAY, (void *)&value, sizeof(value));
 }
 
-// 获取指定socket上的错误码,如果socket上无错误应返回0
+int m_set_ipv6_only(int socket, int value)
+{
+    return setsockopt(socket, IPPROTO_IPV6, IPV6_V6ONLY, (void *)&value, sizeof(value));
+}
+
 int m_get_err_opt(int socket, int *ptr)
 {
     socklen_t ptr_size = sizeof(int);
     return getsockopt(socket, SOL_SOCKET, SO_ERROR, (void *)ptr, &ptr_size);
 }
 
-// 设置socket为非阻塞,失败则返回-1,成功则返回0
 int m_set_nonblocking(int socket)
 {
     int flag = fcntl(socket, F_GETFD, 0);
     return fcntl(socket, F_SETFL, flag | O_NONBLOCK);
 }
 
-// bind端口地址,失败则返回-1,成功则返回0
-int m_bind(int socket, struct sockaddr_in *sockAddr, socklen_t size)
+int m_bind(int socket, void *sockAddr, int size)
 {
     return bind(socket, (struct sockaddr *)sockAddr, size);
 }
 
-// 连接指定地址,失败则返回-1,成功则返回0
-int m_connect(int socket, struct sockaddr_in *sockAddr, socklen_t size)
+int m_connect(int socket, void *sockAddr, int size)
 {
     return connect(socket, (struct sockaddr *)sockAddr, size);
 }
 
-// listen端口地址,失败则返回-1,成功则返回0
 int m_listen(int socket, int backlog)
 {
     return listen(socket, backlog);
 }
 
-// 返回EINPROGRESS的值,用于判定非阻塞socket是否在连接中
-int m_err_inprogress()
-{
-    return EINPROGRESS;
-}
-
-ssize_t m_send(int socket, void *buf, size_t len)
-{
-    return send(socket, buf, len, 0);
-}
-
-// 从socket接受数据,失败则返回-1,成功则返回接受的字节数
-ssize_t m_recv(int socket, void *buf, size_t len)
+int m_recv(int socket, void *buf, int len)
 {
     return recv(socket, buf, len, 0);
 }
 
-// 关闭fd,失败则返回-1,成功则返回0
+int m_send(int socket, void *buf, int len)
+{
+    return send(socket, buf, len, 0);
+}
+
 int m_close(int fd)
 {
     return close(fd);
 }
 
-// 关闭socket写端,失败则返回-1,成功则返回0
 int m_shutdown_write(int fd)
 {
     return shutdown(fd, SHUT_WR);
 }
 
-// 返回当前错误码
 int m_errno()
 {
     return errno;
--- src/lib_macos.h
+++ src/lib_macos.h
@@ -5,23 +5,39 @@ EXPORT_SYMBOL int m_connect_block_code();
 
 EXPORT_SYMBOL int m_send_block_code();
 
+EXPORT_SYMBOL int m_interrupt_code();
+
+EXPORT_SYMBOL int m_ipv4_address_len();
+
+EXPORT_SYMBOL int m_ipv6_address_len();
+
 EXPORT_SYMBOL int m_kqueue();
 
 EXPORT_SYMBOL int m_kevent_ctl(int kq, int fd, int16_t filter, uint16_t flags);
 
 EXPORT_SYMBOL int m_kevent_wait(int kq, struct kevent *eventlist, int nevents, struct timespec *timeout);
 
-EXPORT_SYMBOL socklen_t m_address_len();
+EXPORT_SYMBOL int m_ipv4_address_size();
 
-EXPORT_SYMBOL int m_address(struct sockaddr_in *sockAddr, char *addrStr, socklen_t len);
+EXPORT_SYMBOL int m_ipv6_address_size();
 
-EXPORT_SYMBOL uint16_t m_port(struct sockaddr_in *sockAddr);
+EXPORT_SYMBOL int m_get_ipv4_address(struct sockaddr_in *sockAddr, char *addrStr, socklen_t len);
 
-EXPORT_SYMBOL int m_socket_create();
+EXPORT_SYMBOL int m_get_ipv6_address(struct sockaddr_in6 *sockAddr, char *addrStr, socklen_t len);
 
-EXPORT_SYMBOL int m_accept(int socket, struct sockaddr_in *clientAddr, socklen_t clientAddrSize);
+EXPORT_SYMBOL uint16_t m_ipv4_port(struct sockaddr_in *sockAddr);
 
-EXPORT_SYMBOL int m_set_sock_addr(struct sockaddr_in *sockAddr, char *address, uint16_t port);
+EXPORT_SYMBOL uint16_t m_ipv6_port(struct sockaddr_in6 *sockAddr);
+
+EXPORT_SYMBOL int m_ipv4_socket_create();
+
+EXPORT_SYMBOL int m_ipv6_socket_create();
+
+EXPORT_SYMBOL int m_accept(int socket, void *clientAddr, socklen_t clientAddrSize);
+
+EXPORT_SYMBOL int m_set_ipv4_sock_addr(struct sockaddr_in *sockAddr, char *address, uint16_t port);
+
+EXPORT_SYMBOL int m_set_ipv6_sock_addr(struct sockaddr_in6 *sockAddr, char *address, uint16_t port);
 
 EXPORT_SYMBOL int m_set_reuse_addr(int socket, int value);
 
@@ -29,21 +45,21 @@ EXPORT_SYMBOL int m_set_keep_alive(int socket, int value);
 
 EXPORT_SYMBOL int m_set_tcp_no_delay(int socket, int value);
 
+EXPORT_SYMBOL int m_set_ipv6_only(int socket, int value);
+
 EXPORT_SYMBOL int m_get_err_opt(int socket, int *ptr);
 
 EXPORT_SYMBOL int m_set_nonblocking(int socket);
 
-EXPORT_SYMBOL int m_bind(int socket, struct sockaddr_in *sockAddr, socklen_t size);
+EXPORT_SYMBOL int m_bind(int socket, void *sockAddr, int size);
+
+EXPORT_SYMBOL int m_connect(int socket, void *sockAddr, int size);
 
 EXPORT_SYMBOL int m_listen(int socket, int backlog);
 
-EXPORT_SYMBOL int m_err_inprogress();
+EXPORT_SYMBOL int m_recv(int socket, void *buf, int len);
 
-EXPORT_SYMBOL int m_connect(int socket, struct sockaddr_in *sockAddr, socklen_t size);
-
-EXPORT_SYMBOL ssize_t m_send(int socket, void *buf, size_t len);
-
-EXPORT_SYMBOL ssize_t m_recv(int socket, void *buf, size_t len);
+EXPORT_SYMBOL int m_send(int socket, void *buf, int len);
 
 EXPORT_SYMBOL int m_close(int fd);
 
